cc2b5f97c4cb2a9a9974010d57bc69e797042c6c
Mark Abuyabor <mark@zeguestlist.com>
2022-08-15 12:58:27 +0800

    json server installed and added

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.blogs');
+
+
+const renderPosts = async () => {
+    let uri = 'http://localhost:3000/posts/';
+
+    const res = await fetch(uri);
+    const post = await res.json();
+    
+    let template = '';
+    post.forEach(post => {
+        template += `
+            <div class="post">
+                <h2>${post.title}</h2>
+                <p><small>${post.likes} likes</small></p>
+                <p>${post.body.slice(0,70)}</p>
+                <a href="/details.html">Read more...</a>
+            </div>
+        `
+    });
+
+    container.innerHTML = template;
+
+
+
+};
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
+
