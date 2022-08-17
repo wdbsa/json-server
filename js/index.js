96c15be658359e4e6a103f325556d2562bb72b4b
Mark Abuyabor <mark@zeguestlist.com>
2022-08-17 22:22:30 +0800

    full search term

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 const container = document.querySelector('.blogs');
+const searchForm = document.querySelector('.search');
 
 
-const renderPosts = async () => {
+const renderPosts = async (term) => {
     let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+
+    if(term) {
+        uri += `&q=${term}`;
+    }
 
     const res = await fetch(uri);
     const post = await res.json();
@@ -24,6 +29,12 @@ const renderPosts = async () => {
 
 
 };
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    renderPosts(searchForm.term.value.trim());
+});
 
 window.addEventListener('DOMContentLoaded', () => renderPosts());
 
